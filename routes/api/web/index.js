4176a90f5bad26d90a553ec9b63d095071cecd43
firdavsokilov <frdavsokilov04@gmail.com>
2024-03-14 14:31:51 +0500

    Deltion

--- routes/api/web/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const home_router = require('./home')
-
-const router = express.Router()
-
-// registering child routers
-router.use('/', home_router)
-
-module.exports = router
